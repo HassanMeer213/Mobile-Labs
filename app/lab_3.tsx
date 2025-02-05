87fe6c5ee5a9093c267077421a3cb82aa484a545
FK323 <166413925+FK323@users.noreply.github.com>
2025-02-05 09:12:36 -0700

    Update lab_3.tsx

--- app/lab_3.tsx
+++ app/lab_3.tsx
@@ -1,1 +1,17 @@
 import React, { useState } from "react";
+import {Button, Text ,View} from "react-native";
+import Decrement from "../components/decrement";
+import Increment from "../components/increment";
+
+const Lab3 = () => {
+  const [value, setValue] =useState<number>(0);
+  return (
+    <View>
+      <Text>My Value: {value} </Text>
+      <Increment value={value} setValue={setValue}/>
+      <Decrement value={value} setValue={setValue}/>
+    </View>
+  );
+};
+
+export default Lab3;
