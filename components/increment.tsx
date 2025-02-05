9b13632ddbc27f82ee82422a58ec2754d15644f3
FK323 <166413925+FK323@users.noreply.github.com>
2025-02-05 09:02:03 -0700

    Update increment.tsx

--- components/increment.tsx
+++ components/increment.tsx
@@ -1,1 +1,19 @@
+import React from "react";
+import {text, view} from "react-native";
 
+type IncrementProps = {value: number: setValue: (value: number) => void};
+
+const Increment: React.FC<IncrementProps> = ({ value, setValue}) => {
+  const handleIncrement = () => {
+    setValue{value + 1};
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={handleIncrement}>
+        <Text>Increment</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default Incerment;
