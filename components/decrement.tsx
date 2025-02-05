53a9cefa3827f749e25d763b930894b43081faf0
Imadrana <imadrana@gmail.com>
2025-02-05 08:59:29 -0700

    Update decrement.tsx

--- components/decrement.tsx
+++ components/decrement.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+import ( Text, TouchableOpacity, View › from "react-native";
 
+type IncrementProps = { value: number; setValue: (value: number) => void };
+                       
+const Decrement: = () =>{
+  return(
+    <View>
+      <TouchalbeOpacity onPress={() => console.log("Decrement")}>
+        <Text>I am decrementing</Text>
+      <TouchableOpacity>
+    </View>
+  );
+};
+
+export default Decrement;
